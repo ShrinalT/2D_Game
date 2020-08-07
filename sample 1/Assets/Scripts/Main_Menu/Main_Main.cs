51582f98929ab336289b3a0fd38d7ce8038bace0
Shrinal Thakkar <44691127+hawkmak3r@users.noreply.github.com>
2020-08-06 22:35:31 -0400

    day 2 end push

---
+++ sample 1/Assets/Scripts/Main_Menu/Main_Main.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_Main : MonoBehaviour
+{
+
+    public void LoadScence()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
+
